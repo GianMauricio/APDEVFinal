610a7235523d2d36d8bfe86d7bdf9f1fec5c0bf4
Intese Avarice <gian_mauricio@dlsu.edu.ph>
2020-09-30 21:22:25 +0800

    Added checker logic to board repsawn

--- FuckingSaveMePlease/src/components/Countdown.js
+++ FuckingSaveMePlease/src/components/Countdown.js
@@ -5,11 +5,10 @@ class Countdown extends cc.Component{
         this.startTime = 7200;
         this.time = clock;
         this.timeStart  = false;
-        this.timeStop = false;
     }
     
     update(dt){
-        if(this.timeStart && this.timeStop == false){
+        if(this.timeStart){
             this.seconds = this.startTime / 60;
             this.getOwner().setTime(this.seconds);
             this.startTime = this.startTime - 1;
@@ -24,9 +23,5 @@ class Countdown extends cc.Component{
     
     go(){
         this.timeStart = true;
-    }
-
-    stop(){
-        this.timeStop = true;
     }
 }
--- FuckingSaveMePlease/src/layouts/GameLayout.js
+++ FuckingSaveMePlease/src/layouts/GameLayout.js
@@ -7,14 +7,7 @@ class GameLayout extends ccui.RelativeBox{
         this.addComponent(new FitToWindow());
         this.addComponent(new EnableOnLandscape());
         this.addComponent(new GameLayerResizer());
-        
-        //SCORE STUFF
-        this.points = 0;
-          //Score stuff
-        this.points = 0;
-        this.scoreStr = "Score: " + this.points.toString();
-        this.score = new ccui.Text(this.scoreStr, "Pixel", 36);
-
+    
         //Timer stuff
         this.clock = new ccui.Text("120", 'Pixel', 36);
         this.addChild(this.clock);
@@ -38,19 +31,10 @@ class GameLayout extends ccui.RelativeBox{
     onEnter(){
         //Timer setup
         super.onEnter();
-        //CLOCK POS
         let clockPos = new ccui.RelativeLayoutParameter();
         clockPos.setAlign(ccui.RelativeLayoutParameter.PARENT_TOP_LEFT);
         this.clock.setLayoutParameter(clockPos);
         this.clock.setAnchorPoint(cc.p(0.0, 0.9));
-
-
-        //SCORE POS
-        let layoutParameter = new ccui.RelativeLayoutParameter();
-        layoutParameter.setAlign(ccui.RelativeLayoutParameter.PARENT_TOP_RIGHT);
-        this.score.setLayoutParameter(layoutParameter);
-        this.score.setAnchorPoint(cc.p(0.5,0.9));
-        this.addChild(this.score);
         this.timer.go();
         
         //Title setup
